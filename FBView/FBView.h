99fe55fa10add3b3833a8ab00b6f7772a575e002
Felip38rito <felipe.correia.wd@gmail.com>
2020-06-09 20:29:02 -0300

    First structure for FBView

---
+++ FBView/FBView.h
@@ -0,0 +1,19 @@
+//
+//  FBView.h
+//  FBView
+//
+//  Created by Felipe Correia on 09/06/20.
+//  Copyright © 2020 felip38rito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBView.
+FOUNDATION_EXPORT double FBViewVersionNumber;
+
+//! Project version string for FBView.
+FOUNDATION_EXPORT const unsigned char FBViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBView/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FBView' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FBView
+
+  target 'FBViewTests' do
+    # Pods for testing
+  end
+
+end
